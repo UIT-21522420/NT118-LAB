b82b1ff531c3cd10bc7632212e2266e4ad499f0e
UIT-21522420 <116177507+UIT-21522420@users.noreply.github.com>
2024-04-23 12:54:44 +0700

    Add lab4

---
+++ Lab4/Lab4_01/app/src/main/java/com/example/lab4_01/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab4_01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
